721bbfca0b43b8ce16620c6fad7ffb04f83c8c1b
jazellemaira <jazellemaira@pm.me>
2024-06-28 16:35:08 +0200

    Setting snake movement loop

--- snake.js
+++ snake.js
@@ -1,15 +1,26 @@
 export const SNAKE_SPEED = 2
-const snakeBody = [{ x: 11, y: 11 }]
+const snakeBody = [
+  { x: 10, y: 11 },
+  { x: 11, y: 11 },
+  { x: 12, y: 11 },
+  { x: 13, y: 11 },
+  { x: 14, y: 11 },
+]
 
 export function update() {
-  console.log("update snake")
+  for (let i = snakeBody.length - 2; i >= 0; i--) {
+    snakeBody[i + 1] = { ...snakeBody[i] }
+  }
+
+  snakeBody[0].x += 0
+  snakeBody[0].y += 1
 }
 
 export function draw(gameBoard) {
   snakeBody.forEach((segment) => {
     const snakeElement = document.createElement("div")
-    snakeElement.style.gridRowStart = segment.x
-    snakeElement.style.gridColumnStart = segment.y
+    snakeElement.style.gridRowStart = segment.y
+    snakeElement.style.gridColumnStart = segment.x
     snakeElement.classList.add("snake")
     gameBoard.appendChild(snakeElement)
   })
