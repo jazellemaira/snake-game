39bd7b4b163d01141667b76f1167743c5e76f332
jazellemaira <jazellemaira@pm.me>
2024-06-28 22:39:56 +0200

    Adding game over conditions

--- game.js
+++ game.js
@@ -2,13 +2,25 @@ import {
   SNAKE_SPEED,
   update as updateSnake,
   draw as drawSnake,
+  getSnakeHead,
+  snakeIntersection,
 } from "./snake.js"
 import { update as updateFood, draw as drawFood } from "./food.js"
+import { outsideGrid } from "./grid.js"
 
 let lastRenderTime = 0
+let gameOver = false
 const gameBoard = document.querySelector("#game-board")
 
+// Main game loop
 function main(currentTime) {
+  if (gameOver) {
+    if (confirm("You lost! Press ok to restart.")) {
+      window.location = "/"
+    }
+    return
+  }
+
   window.requestAnimationFrame(main)
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
@@ -24,9 +36,15 @@ window.requestAnimationFrame(main)
 function update() {
   updateSnake()
   updateFood()
+  checkDeath()
 }
+
 function draw() {
   gameBoard.innerHTML = ""
   drawSnake(gameBoard)
   drawFood(gameBoard)
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
